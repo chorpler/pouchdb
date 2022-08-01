c92e8de95779830a28b011111fd19a721f125b67
David Sargeant <chorpler@gmail.com>
2022-07-31 19:57:04 -0500

    Try webpack 4

--- bin/test-webpack.sh
+++ bin/test-webpack.sh
@@ -7,7 +7,8 @@
 #
 
 npm run build
-npm install --save-dev webpack@1.13.1 # do this on-demand to avoid slow installs
+# npm install --save-dev webpack@1.13.1 # do this on-demand to avoid slow installs
+npm install --save-dev webpack@4 # do this on-demand to avoid slow installs
 node bin/update-package-json-for-publish.js
 ./node_modules/.bin/webpack \
   --output-library PouchDB --output-library-target umd \
